97ce0838aa301ed013b91ce328e59049a9a9a9a0
SirGouki <sirgouki@gmail.com>
2020-01-24 02:03:34 -0600

    added MobMods to make Save the Children act like retail

--- scripts/zones/Ghelsba_Outpost/mobs/Fodderchief_Vokdek.lua
+++ scripts/zones/Ghelsba_Outpost/mobs/Fodderchief_Vokdek.lua
@@ -8,6 +8,10 @@ require("scripts/globals/status");
 
 function onMobInitialize(mob)
     mob:setMobMod(dsp.mobMod.GIL_MAX, -1);
+    mob:setMobMod(dsp.mobMod.SIGHT_RANGE, 50);
+	mob:setMobMod(dsp.mobMod.ALWAYS_AGGRO, 1);
+	mob:setMobMod(dsp.mobMod.MUG_GIL, -1);
+	mob:setMobMod(dsp.mobMod.NO_DESPAWN, 1);
 end;
 
 function onMobDeath(mob, player, isKiller)
